623521b78a075a5364f90b1501ace476e9ee2106
Fabian Mardones <fabianmardones94@gmail.com>
2024-01-14 16:13:41 -0300

    contador modificado

--- public/js/contador.js
+++ public/js/contador.js
@@ -1,33 +1,39 @@
-const contador = document.querySelector('#contador')
+// const contador = document.querySelector('#contador')
 
-const url = 'http://127.0.0.1:3001/contadorDeAlmas'
+// const url = 'http://127.0.0.1:3001/contadorDeAlmas'
 
-const mostrarConteo = async() => {
-    try {
-        const response = await fetch(url)
-        const data = await response.json()
-        return data
+// const mostrarConteo = async() => {
+//     try {
+//         const response = await fetch(url)
+//         const data = await response.json()
+//         return data
 
-    } catch (error) {
-        console.log(error);
-    }
-}
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
-document.addEventListener('DOMContentLoaded', () => {
-  mostrarConteoDom()
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   mostrarConteoDom()
+// })
 
 
 
-async function mostrarConteoDom(){
-    const datos = await mostrarConteo()
+// async function mostrarConteoDom(){
+//     const datos = await mostrarConteo()
 
-    console.log(datos);
+//     console.log(datos);
 
-    const { results } = datos 
+//     const { results } = datos 
 
-    const numero = document.createElement('h1')
-    numero.textContent = results
+//     const numero = document.createElement('h1')
+//     numero.textContent = results
 
-    contador.appendChild(numero)
-}
+//     contador.appendChild(numero)
+// }
+
+
+const text = document.querySelector('.text p')
+text.innerHTML = text.innerText.split('').map((char, i) => 
+    `<span style="transform:rotate(${i * 8.3}deg)">${char}</span>`
+).join('')
